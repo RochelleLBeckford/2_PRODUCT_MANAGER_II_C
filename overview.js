82e8aea1c3c56eaac3ba33b84e58225a84ab8b4a
Rochelle L Beckford <112206517+RochelleLBeckford@users.noreply.github.com>
2023-01-30 19:15:54 -0500

    Update overview.js

--- overview.js
+++ overview.js
@@ -1,31 +1,60 @@
 /*
-& create React App 
+& Create server folder - Back End
+~ npm init -y 
+-> Initiate project -> will create package.json 
+
+? Install the dependencies 
+~ npm install mongoose express cors dotenv
+-> dotenv -> so we can out our ports in our environment variables
+
+-> cors -> cross origin request -> send a request that is not on our local host
+-> by defualt our server will block anything not coming from our server
+-> need to use a middle ware method to make cors btw express and react
+-> whenever need info from the backend we will use this technique
+
+& Can create the other folders in terminal
+-> cd server
+-> ls -> to see the current folders and files in there
+-> mkdir routes models config controllers
+-> now the other folders should appear in server
+
+
+//******************************************************
+& Create React App - Front-End
 -> in the terminal of where I want the folder to be install react
--> npx create-react-app prop_it_up(what i want to name the folder that will be housing react)
--> after load -> cd to whatever the folder name is -> client
+-> npx create-react-app client(what i want to name the folder that will be housing react)
+-> after load -> cd client
+?Install dependencies 
+-> npm i axios react-router-dom
+    -> check package.json -> axios, react, react-router-dom in dependencies
 -> npm start to run react app
+
 
 & Change the App.js and index.js file name to jsx so can utilize all the emmet in the src folder
 -> will have to reload the terminal since I changed the file names to jsx and then the react app will appear again
-~ Folders w/i
+~ index.jsx
+-> import { BrowserRouter } from react-router-dom
+
+~ Create Folder w/i src
 -> components
--> pages
+
 
 & Add bootstrap for css and js to index.html in public folder 
--> or bootswatch and place it in src folder
+-> or bring in downloaded file for bootswatch -> place it in src folder
+-> impoort into App.jsx 
+-> import './bootstrap.min.css';
 
-& Create server folder
-~ File/Folders w/i
--> server.js
--> config
--> controllers
--> models
--> routes
+& Project will have 2 folders
+-> client folder is for React
+-> server folder is for Express
 
-& Install the dependencies 
-~npm init -y
-~npm install mongoose express
+* note -> -> to see what version of Mongo I am on -> in terminal:
+-> mongod --version
 
+* note -> if right click in src and say new file 
+-> type components/Home.jsx 
+-> new folder component and new file Home.jsx will auto create 
 
-
+* note -> to scale up from your current location in the terminal 
+-> cd .. -> hit enter
 */
